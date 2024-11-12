1ca2ecdc6618909b185e84e04ae97b55d7ece365
MEJSON <98404892+MEJSONN@users.noreply.github.com>
2024-11-12 11:50:03 +0100

    Update index.php

--- 12.11.2024/index.php
+++ 12.11.2024/index.php
@@ -10,9 +10,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <style>
+        body {background-color: black;}
         .main { display: flex; }
-        .left { flex: 1; background-color: lime; padding: 5px;}
-        .right{ flex:2; background-color: yellow; padding: 5px;}
+        .left { flex: 1; background-color: grey; padding: 5px;}
+        .right{ flex:2; background-color: darkgrey; padding: 5px;}
+        
     </style>
 </head>
 <body>
@@ -21,6 +23,18 @@
         <form method="POST" action='zapisz.php'>
             <input name='miasto' />
             <input type='submit' name='sub' value='Dodaj'/>
+        </form>
+        <form class="car" method="POST" action="zapisz.php"><br>
+            <input type="text" name="marka" placeholder="marka"><br>
+            <input type="text" name="kolor" placeholder="kolor"><br>
+            <input type="text" name="nr_rej" placeholder="nr_rej"><br>
+            <input type="text" name="przebieg" placeholder="przebieg"><br>
+            <input type="text" name="cena_pln" placeholder="cena_pln"><br>
+            <input type="text" name="cena_eu" placeholder="cena_eu"><br>
+            <input type="text" name="rok_prod" placeholder="rok_prod"><br>
+            <input type="text" name="pojemnosc" placeholder="pojemnosc"><br>
+            <input type="text" name="paliwo" placeholder="paliwo"><br>
+            <input type='submit' name='sub2' value='Dodaj'/><br>
         </form>
     </div>
     <div class='right'>
