3e0d3c0b9c8ec4107d773ce788c268db680d3625
MEJSON <98404892+MEJSONN@users.noreply.github.com>
2024-11-13 08:54:35 +0100

    Update 12.11.2024 Q1.sql

--- 12.11.2024 Q1.sql
+++ 12.11.2024 Q1.sql
@@ -15,3 +15,4 @@
 15. SELECT auto.*, komis.miasto FROM `umowa` LEFT JOIN auto on auto.id=umowa.auto_id LEFT JOIN komis on komis.id=umowa.komis_id WHERE auto.marka='BMW'
 16. SELECT count(umowa.kupujacy_id) AS liczba_kupionych, osoba.i, osoba.n FROM `umowa` LEFT JOIN osoba on osoba.id=umowa.kupujacy_id GROUP BY osoba.i, osoba.n
 17. SELECT count(umowa.komis_id) AS liczba_sprzedanych, komis.miasto FROM `umowa` LEFT JOIN komis on komis.id=umowa.komis_id GROUP BY komis.miasto
+!! 18. SELECT DISTINCT komis.miasto, umowa.id FROM `umowa` LEFT JOIN komis on komis.id=umowa.komis_id ORDER BY umowa.id DESC;
